3faadbfe24e5ed77e6709e04fbfc4f17a37919e0
ideepakrajput <deepakkumar18112001@gmail.com>
2024-09-19 22:58:21 +0530

    cors fixed

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+	{
+		origin: '*',
+	}
+));
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
