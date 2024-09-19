c707391d3a1d61150cc2ec210d4991a44c06b6b2
ideepakrajput <deepakkumar18112001@gmail.com>
2024-09-19 22:39:14 +0530

    Completed

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 3177;
+
+dotenv.config();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
+	console.log(e ? e : "Connected successfully to database");
+});
+
+app.use("/auth", require("./routers/authRouter"));
+app.use("/user", require("./routers/userRouter"));
+app.use("/bank", require("./routers/bankRouter"));
+app.use("/camps", require("./routers/campRouter"));
+
+app.listen(port, () =>
+	console.log(`Server running at http://localhost:${port}`)
+);
