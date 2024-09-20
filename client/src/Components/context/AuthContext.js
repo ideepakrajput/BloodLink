08b969896f4d10d6956f5b2a559eecda7d6ee668
ideepakrajput <deepakkumar18112001@gmail.com>
2024-09-20 20:56:25 +0530

    backend url fixed

--- client/src/Components/context/AuthContext.js
+++ client/src/Components/context/AuthContext.js
@@ -8,7 +8,7 @@ function AuthContextProvider(props) {
   const [user, setUser] = useState([]);
 
   async function getLoggedIn() {
-    const loggedInRes = await axios.get("http://localhost:3177/auth/loggedIn", { withCredentials: true });
+    const loggedInRes = await axios.get("https://bloodlink-fjkq.onrender.com/auth/loggedIn", { withCredentials: true });
     setLoggedIn(loggedInRes.data.auth);
     setUser(loggedInRes.data.user);
   }
