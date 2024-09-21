04c5da32e03f5f2f1b7f3c464700ded678773604
ideepakrajput <deepakkumar18112001@gmail.com>
2024-09-21 15:24:46 +0530

    origin fixed

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,19 @@ const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
 
-const app = express();
 const port = 3177;
-
 dotenv.config();
+const app = express();
+
+app.use(cors(
+	{
+		origin: "https://bloodlink-fjkq.onrender.com/",
+		credentials: true,
+	}
+));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors(
-	{
-		origin: '*',
-	}
-));
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
